3b4db295ad399e1098cbcb21919c53ffcbf77544
Abdullah Aghazadah <abdullah.aghazadah@gmail.com>
2016-11-08 06:16:31 +0000

    Improve DialogGui documentation

--- DialogGui.h
+++ DialogGui.h
@@ -13,6 +13,9 @@ class ScrollWindow;
 class Label;
 class QGraphicsItem;
 
+/// Represents a choice that can be presented in a DialogGui when a certain
+/// response is presented.
+/// @see Response, DialogGui
 class Choice {
 public:
     Choice(const std::string& text);
@@ -21,6 +24,10 @@ private:
     std::string text_;
 };
 
+/// Represents a response that can be presented in a DialogGui when a certain
+/// choice is selected (or when no choice is selected - the default displayed
+/// Response).
+/// @see Choice, DialogGui
 class Response{
 public:
     Response(const std::string& text);
@@ -30,11 +37,14 @@ private:
 };
 
 /// Represents a Gui that can be used to present an interactive dialog.
+///
 /// Initially a "response" is displayed with all the "choices" for that response.
 /// When a certain choice is clicked, the response of that choice will be displayed,
-/// along with all of the choices for that response.
+/// along with all of the choices for that new response.
+///
 /// So each response has a bunch of choices that are displayed when that response is
 /// displayed and each choice has a response that it will display when its clicked.
+///
 /// You can add a response via addResponse(Response* response).
 /// You can add a choices for a response via addChoice(Response* forResponse, Choice* choice).
 /// You can set the associated response that will be shown when a certain
