6e1ab07a233e4dd073368909d8604d19c92eedd8
Abdullah Aghazadah <abdullah.aghazadah@gmail.com>
2017-11-26 05:31:46 +0000

    Add a class for generating random ints, doubles, and points within some range.

---
+++ RandomGenerator.h
@@ -0,0 +1,19 @@
+#pragma once
+
+class QPointF;
+class QRectF;
+
+/// Allows generating random numbers/points.
+/// @author Abdullah Aghazadah
+/// @date 11/25/17
+class RandomGenerator
+{
+public:
+    RandomGenerator();
+
+    int randInt(int min, int max);
+    int randDouble(double start, double end);
+    QPointF randQPointF(const QPointF& topLeft, const QPointF& botRight);
+    QPointF randQPointF(const QRectF& inRegion);
+};
+
